1c8e23457cf177baafaaf5f1091414fc0bde5cde
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-08 23:03:42 +0300

    InitCommit

---
+++ BasicsOfProgramming/Naming/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Naming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 100;
+            float damageInPercent = 0.1f;
+            bool isAlive;
+            string petName = "Doggy";
+            uint coins = 40;
+            double accuracyOfSolution = 10E-8;
+            char filteringLetter = 'N';
+            ulong key;
+            byte colorMask;
+            bool isAvailableForPayment;
+        }
+    }
+}
