e64cd827e2442ab470a6277fbcd04300dc68f490
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-08 23:16:56 +0300

    TextAboutUser programm

---
+++ BasicsOfProgramming/TextAboutUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TextAboutUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Какая твоя любимая игра?");
+            string favoriteGame = Console.ReadLine();
+            Console.WriteLine("Сколько часов ты в нее наиграл?");
+            int amountOfHoursPlayed = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Привет {name}, я тоже люблю играть в {favoriteGame} и я рад что ты наиграл {amountOfHoursPlayed} часов в эту замечательную игру!");
+        }
+    }
+}
