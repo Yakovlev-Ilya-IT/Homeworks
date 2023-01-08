3502615c8b347b7714130af5d127b0d32c3e1dc3
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-08 23:28:25 +0300

    PicturesOnDisplay programm

---
+++ BasicsOfProgramming/PicturesOnDisplay/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PicturesOnDisplay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxPicturesInRow = 3;
+            int totalPictures = 52;
+
+            int numberOfFilledRows = totalPictures / maxPicturesInRow;
+            int numberOfExtraImages = totalPictures % maxPicturesInRow;
+
+            Console.WriteLine($"Количество заполненных рядов: {numberOfFilledRows}");
+            Console.WriteLine($"Количество лишних картинок: {numberOfExtraImages}");
+        }
+    }
+}
