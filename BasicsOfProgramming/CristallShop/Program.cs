1913547d5347f36dadcd2d2cd91cf075f2a3d58c
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 20:24:24 +0300

    CrystallShop programm

---
+++ BasicsOfProgramming/CristallShop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CristallShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint crystalCost = 10;
+
+            Console.WriteLine($"Привет, один кристалл в нашей лавке стоит {crystalCost} золота");
+            Console.WriteLine("Сколько у тебя сейчас золота?");
+
+            uint currentGold = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("Отлично, сколько кристаллов ты хочешь купить?");
+
+            uint numberOfCrystalsToBuy = uint.Parse(Console.ReadLine());
+
+            currentGold -= numberOfCrystalsToBuy * crystalCost;
+
+            Console.WriteLine($"Теперь у тебя {numberOfCrystalsToBuy} кристаллов и {currentGold} золота");
+        }
+    }
+}
