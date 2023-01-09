9f8ee6460836795f3386a04c73c9f459176c1dd4
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 21:31:16 +0300

    WhileExit programm

---
+++ ConditionalOperatorsAndCycle/WhileExit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WhileExit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCondition = "exit";
+            string userInput;
+
+            do
+            {
+                Console.WriteLine("Введите exit, что бы завершить программу: ");
+                userInput = Console.ReadLine();
+
+            } while (userInput != exitCondition);
+        }
+    }
+}
