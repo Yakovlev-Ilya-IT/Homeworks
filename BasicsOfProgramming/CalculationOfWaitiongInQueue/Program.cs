bba5b844f9da50ea01f575f0ed78b144d9d6db5f
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 20:36:07 +0300

    CalculationOfWaitiongInQueue programm

---
+++ BasicsOfProgramming/CalculationOfWaitiongInQueue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalculationOfWaitiongInQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint timeForServicePerPeopleInMinutes = 10;
+            uint numberOfMinutesInHour = 60;
+
+            Console.Write("Введите кол-во людей в очереди: ");
+
+            uint peopleNumber = uint.Parse(Console.ReadLine());
+
+            uint waitingTimeInMinutes = peopleNumber * timeForServicePerPeopleInMinutes;
+            uint numberOfFullWaitingHours = waitingTimeInMinutes / numberOfMinutesInHour;
+            uint numberOfRemainingWaitingMinutes = waitingTimeInMinutes % numberOfMinutesInHour;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {numberOfFullWaitingHours} часа и {numberOfRemainingWaitingMinutes} минут.");
+        }
+    }
+}
