7cdb291e7f5a5dcfb06f4e65b8724bafd0f5a8de
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 20:14:43 +0300

    Swiping values programm

---
+++ BasicsOfProgramming/SwappingValues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwappingValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Яковлев";
+            string surname = "Илья";
+
+            Console.WriteLine("Значения до перестановки:");
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Фамилия: {surname}");
+
+            string clipboard = name;
+            name = surname;
+            surname = clipboard;
+
+            Console.WriteLine("Значения после перестановки:");
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Фамилия: {surname}");
+        }
+    }
+}
