1de94d83aa55a6a194d9bacbafec3e20bc734b3f
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 21:45:39 +0300

    SequenceOfNumbers programm

---
+++ ConditionalOperatorsAndCycle/SequenceOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SequenceOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 5;
+            int lastNumber = 96;
+            int step = 7;
+
+            for (int currentNumber = firstNumber; currentNumber <= lastNumber; currentNumber += step)
+            {
+                Console.WriteLine(currentNumber);
+            }
+        }
+    }
+}
