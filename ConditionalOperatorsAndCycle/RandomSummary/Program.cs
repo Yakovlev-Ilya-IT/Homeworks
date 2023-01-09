7bf1ed388a1762c11d016ae2082ea2d3ec48b579
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 22:13:15 +0300

    RandomSummary programm

---
+++ ConditionalOperatorsAndCycle/RandomSummary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RandomSummary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerBound = 0;
+            int upperBound = 101;
+
+            int firstDivisor = 3;
+            int secondDivisor = 5;
+
+            Random random = new Random();
+            int number = random.Next(lowerBound, upperBound);
+
+            Console.WriteLine($"Полученое число: {number}");
+
+            int sum = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % firstDivisor == 0 || i % secondDivisor == 0)
+                    sum += i;
+            }
+
+            Console.WriteLine($"Сумма: {sum}");
+        }
+    }
+}
