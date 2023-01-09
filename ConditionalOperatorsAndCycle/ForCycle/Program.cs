e77da07ec85acbd93110ab49153c5ba6c79e2e42
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-09 21:23:28 +0300

    ForCycle programm

---
+++ ConditionalOperatorsAndCycle/ForCycle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите сообщения для повтора:");
+
+                string message = Console.ReadLine();
+
+                Console.WriteLine("Введите количество повторений:");
+
+                int numberOfRepetitions = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < numberOfRepetitions; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
