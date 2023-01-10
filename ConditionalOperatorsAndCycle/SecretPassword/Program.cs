c082e951ac9f56da6232ef26b8d7c4d1741bb174
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-10 22:48:44 +0300

    SecretPassword programm

---
+++ ConditionalOperatorsAndCycle/SecretPassword/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SecretPassword
+{
+    internal class Program
+    {
+        const int NumberOfTries = 3;
+        const string SecretMessage = "А ты хороооош мужик... хорооош!";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первоначальный пароль: ");
+            string password = Console.ReadLine();
+
+            for (int i = 0; i < NumberOfTries; i++)
+            {
+                Console.WriteLine("Введите пароль: ");
+                string userInput = Console.ReadLine();
+
+                if(userInput == password)
+                {
+                    Console.WriteLine(SecretMessage);
+                    break;
+                }
+
+                int remainingAttempts = NumberOfTries - (i + 1);
+
+                if(remainingAttempts <= 0)
+                {
+                    Console.WriteLine("так и знал, что ты вор, а ну вон отсюда!");
+                    break;
+                }
+
+                Console.WriteLine($"Пароль неверный, оставшееся количество попыток: {remainingAttempts}");
+            }
+        }
+    }
+}
