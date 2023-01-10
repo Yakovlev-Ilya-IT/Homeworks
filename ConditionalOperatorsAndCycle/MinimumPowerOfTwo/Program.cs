d30fb505e05db456bbcd8552e7d24d75e21e5b44
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-10 23:18:47 +0300

    MinimumPowerOfTwo programm

---
+++ ConditionalOperatorsAndCycle/MinimumPowerOfTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MinimumPowerOfTwo
+{
+    internal class Program
+    {
+        private const int Basis = 2;
+
+        private const int LowerBoundOfNumberGeneration = 1;
+        private const int UpperBoundOfNumberGeneration = 100;
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(LowerBoundOfNumberGeneration, UpperBoundOfNumberGeneration);
+
+            Console.WriteLine($"Сгенирированное число: {number}");
+
+            int degreeCounter = 0;
+
+            while(number > 0)
+            {
+                number /= Basis;
+                degreeCounter++;
+            }
+
+            int result = (int)Math.Pow(Basis, degreeCounter);
+
+            Console.WriteLine($"Степень: {degreeCounter} \nЧисло в степени: {result}");
+        }
+    }
+}
