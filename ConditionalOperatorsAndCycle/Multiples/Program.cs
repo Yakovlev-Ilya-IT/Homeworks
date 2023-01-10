6f4194059b1087c22c3f53c128fba02a0b07e777
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-10 23:06:53 +0300

    Multiples

---
+++ ConditionalOperatorsAndCycle/Multiples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Multiples
+{
+    internal class Program
+    {
+        private const int LowerBoundOfNumberGeneration = 1;
+        private const int UpperBoundOfNumberGeneration = 28;
+
+        private const int StartOfNumericalInterval = 100;
+        private const int EndOfNumericalInterval = 1000;
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(LowerBoundOfNumberGeneration, UpperBoundOfNumberGeneration);
+
+            Console.WriteLine($"Сгенирированное число: {number}");
+
+            int result = 0;
+
+            for (int i = 0; i < EndOfNumericalInterval; i += number)
+            {
+                if(i >= StartOfNumericalInterval)
+                    result++;
+            }
+
+            Console.WriteLine($"количество трехзначных натуральных чисел, которые кратны {number}: {result}");
+        }
+    }
+}
