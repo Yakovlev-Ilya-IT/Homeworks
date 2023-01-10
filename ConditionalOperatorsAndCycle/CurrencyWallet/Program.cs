a6d03e5fb9d17cffd869801802bc3fcef86adbb4
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-10 21:03:27 +0300

    CurrencyWallet edits

--- ConditionalOperatorsAndCycle/CurrencyWallet/Program.cs
+++ ConditionalOperatorsAndCycle/CurrencyWallet/Program.cs
@@ -4,10 +4,6 @@ namespace CurrencyWallet
 {
     internal class Program
     {
-        const float RubleToDollarExchangeRate = 70.3f, RubleToEuroExchangeRate = 75.08f;
-        const float DollarToRubleExchangeRate = 0.014f, DollarToEuroExchangeRate = 1.08f;
-        const float EuroToRubbleExchangeRate = 0.013f, EuroToDollarExchangeRate = 0.92f;
-
         const string ExitCommand = "exit";
         const string ConvertingRubleToDollarCommand = "1";
         const string ConvertingDollarToRubbleCommand = "2";
@@ -18,13 +14,19 @@ namespace CurrencyWallet
 
         static void Main(string[] args)
         {
+            float rubleToDollarExchangeRate = 70.3f, rubleToEuroExchangeRate = 75.08f;
+            float dollarToRubleExchangeRate = 0.014f, dollarToEuroExchangeRate = 1.08f;
+            float euroToRubbleExchangeRate = 0.013f, euroToDollarExchangeRate = 0.92f;
+
             float rublesInWallet = 1000;
             float dollarsInWallet = 1000;
             float euroInWallet = 1000;
 
+            string userInputCommand;
+
             Console.WriteLine("Добро пожаловать в банк, для выхода наберите команду exit");
 
-            while (true)
+            do
             {
                 Console.WriteLine("Ваш баланс на текущий момент: ");
                 Console.WriteLine($"{rublesInWallet} рублей");
@@ -32,17 +34,14 @@ namespace CurrencyWallet
                 Console.WriteLine($"{euroInWallet} евро");
 
                 Console.WriteLine("Выберите необходимую операцию:");
-                Console.WriteLine("1 - обменять рубли на доллары");
-                Console.WriteLine("2 - обменять доллары на рубли");
-                Console.WriteLine("3 - обменять евро на рубли");
-                Console.WriteLine("4 - обменять рубли на евро");
-                Console.WriteLine("5 - обменять евро на доллары");
-                Console.WriteLine("6 - обменять доллары на евро");
+                Console.WriteLine($"{ConvertingRubleToDollarCommand} - обменять рубли на доллары");
+                Console.WriteLine($"{ConvertingDollarToRubbleCommand} - обменять доллары на рубли");
+                Console.WriteLine($"{ConvertingEuroToRubbleCommand} - обменять евро на рубли");
+                Console.WriteLine($"{ConvertingRubbleToEuroCommand} - обменять рубли на евро");
+                Console.WriteLine($"{ConvertingEuroToDollarCommand} - обменять евро на доллары");
+                Console.WriteLine($"{ConvertingDollarToEuroCommand} - обменять доллары на евро");
 
-                string userInputCommand = Console.ReadLine();
-
-                if (userInputCommand == ExitCommand)
-                    break;
+                userInputCommand = Console.ReadLine();
 
                 float exchangeCurrencyCount;
                 switch (userInputCommand)
@@ -51,84 +50,102 @@ namespace CurrencyWallet
                         Console.WriteLine("Обмен рублей на доллары");
                         Console.WriteLine("Сколько рублей вы хотите обменять?");
                         exchangeCurrencyCount = float.Parse(Console.ReadLine());
+
                         if(rublesInWallet >= exchangeCurrencyCount)
                         {
                             rublesInWallet -= exchangeCurrencyCount;
-                            dollarsInWallet += exchangeCurrencyCount / RubleToDollarExchangeRate;
-                            break;
+                            dollarsInWallet += exchangeCurrencyCount / rubleToDollarExchangeRate;
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас не хватает рублей на счете");
                         }
 
-                        Console.WriteLine("У вас не хватает рублей на счете");
                         break;
                     case ConvertingDollarToRubbleCommand:
                         Console.WriteLine("Обмен долларов на рубли");
                         Console.WriteLine("Сколько долларов вы хотите обменять?");
                         exchangeCurrencyCount = float.Parse(Console.ReadLine());
+
                         if (dollarsInWallet >= exchangeCurrencyCount)
                         {
                             dollarsInWallet -= exchangeCurrencyCount;
-                            rublesInWallet += exchangeCurrencyCount / DollarToRubleExchangeRate;
-                            break;
+                            rublesInWallet += exchangeCurrencyCount / dollarToRubleExchangeRate;
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас не хватает долларов на счете");
                         }
 
-                        Console.WriteLine("У вас не хватает долларов на счете");
                         break;
                     case ConvertingEuroToRubbleCommand:
                         Console.WriteLine("Обмен евро на рубли");
                         Console.WriteLine("Сколько евро вы хотите обменять?");
                         exchangeCurrencyCount = float.Parse(Console.ReadLine());
+
                         if (euroInWallet >= exchangeCurrencyCount)
                         {
                             euroInWallet -= exchangeCurrencyCount;
-                            rublesInWallet += exchangeCurrencyCount / EuroToRubbleExchangeRate;
-                            break;
+                            rublesInWallet += exchangeCurrencyCount / euroToRubbleExchangeRate;
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас не хватает евро на счете");
                         }
 
-                        Console.WriteLine("У вас не хватает евро на счете");
                         break;
                     case ConvertingRubbleToEuroCommand:
                         Console.WriteLine("Обмен рубли на евро");
                         Console.WriteLine("Сколько рублей вы хотите обменять?");
                         exchangeCurrencyCount = float.Parse(Console.ReadLine());
+
                         if (rublesInWallet >= exchangeCurrencyCount)
                         {
                             rublesInWallet -= exchangeCurrencyCount;
-                            euroInWallet += exchangeCurrencyCount / RubleToEuroExchangeRate;
-                            break;
+                            euroInWallet += exchangeCurrencyCount / rubleToEuroExchangeRate;
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас не хватает рублей на счете");
                         }
 
-                        Console.WriteLine("У вас не хватает рублей на счете");
                         break;
                     case ConvertingEuroToDollarCommand:
                         Console.WriteLine("Обмен евро на доллары");
                         Console.WriteLine("Сколько евро вы хотите обменять?");
                         exchangeCurrencyCount = float.Parse(Console.ReadLine());
+
                         if (euroInWallet >= exchangeCurrencyCount)
                         {
                             euroInWallet -= exchangeCurrencyCount;
-                            dollarsInWallet += exchangeCurrencyCount / EuroToDollarExchangeRate;
-                            break;
+                            dollarsInWallet += exchangeCurrencyCount / euroToDollarExchangeRate;
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас не хватает евро на счете");
                         }
 
-                        Console.WriteLine("У вас не хватает евро на счете");
                         break;
                     case ConvertingDollarToEuroCommand:
                         Console.WriteLine("Обмен доллары на евро");
                         Console.WriteLine("Сколько долларов вы хотите обменять?");
                         exchangeCurrencyCount = float.Parse(Console.ReadLine());
+
                         if (dollarsInWallet >= exchangeCurrencyCount)
                         {
                             dollarsInWallet -= exchangeCurrencyCount;
-                            euroInWallet += exchangeCurrencyCount / DollarToEuroExchangeRate;
-                            break;
+                            euroInWallet += exchangeCurrencyCount / dollarToEuroExchangeRate;
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас не хватает долларов на счете");
                         }
 
-                        Console.WriteLine("У вас не хватает долларов на счете");
                         break;
                     default:
                         break;
                 }
-            }
+            } while (userInputCommand != ExitCommand);
         }
     }
 }
