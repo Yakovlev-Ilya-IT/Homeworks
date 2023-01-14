8e7b4ba84409a94300d2cb4f0a38b821a6165492
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-14 19:34:58 +0300

    StringSplit

---
+++ Arrays/StringSplit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Привет, я просто строка со словами, которую нужно покрамсать";
+
+            string[] splitMessage = message.Split(' ');
+
+            Console.WriteLine(message);
+
+            foreach (string item in splitMessage)
+                Console.WriteLine(item);
+        }
+    }
+}
