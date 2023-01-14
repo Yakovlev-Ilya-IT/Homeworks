11d2b76339bdb6f5d2d010d2ff90bbe431977f0b
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-14 18:05:28 +0300

    MaxMatrixElement programm

---
+++ Arrays/MaxMatrixElement/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MaxMatrixElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[10, 10];
+
+            int maxElement = int.MinValue;
+
+            Random random = new Random();
+            int lowerGenerationLimit = 1;
+            int upperGenerationLimit = 10;
+
+            Console.WriteLine("Исходная матрица: ");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(lowerGenerationLimit, upperGenerationLimit);
+                    Console.Write(matrix[i, j] + " ");
+
+                    if(matrix[i, j] > maxElement)
+                        maxElement = matrix[i, j];
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nНаибольший элемент: {maxElement}\n");
+            Console.WriteLine("Полученная матрица: ");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == maxElement)
+                        matrix[i, j] = 0;
+
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
