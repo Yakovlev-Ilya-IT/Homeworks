859620c32ad92a9bc5a90738a399f2d5eb216680
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-01-31 20:59:56 +0300

    ConvertNumber programm

---
+++ Functions/ConvertNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConvertNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет введи число, а я постараюсь его вывести:)");
+
+            int inputNumber = GetNumberFromInput();
+
+            Console.WriteLine($"Твое число: {inputNumber}");
+        }
+
+        static int GetNumberFromInput()
+        {
+            int resultNumber = 0;
+            bool isInputConvertSuccessfull = false;
+
+            while (!isInputConvertSuccessfull)
+            {
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    resultNumber = number;
+                    isInputConvertSuccessfull = true;
+                }
+                else
+                {
+                    Console.WriteLine("Что-то пошло не так, попробуй снова:)");
+                }
+            }
+
+            return resultNumber;
+        }
+    }
+}
