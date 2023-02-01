c3c60dab04c2777eab91026fe373e6e3f49de0d4
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-02-01 19:00:33 +0300

    PlayerInformation programm

---
+++ PlayerInformation/PlayerInformation/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PlayerInformation
+{
+    public class Player
+    {
+        private string _name;
+        private int _power;
+        private int _level;
+        private int _health;
+
+        public Player(string name, int power, int level, int health)
+        {
+            _name = name;
+
+            if(power < 0)
+                _power = 0;
+            else
+                _power = power;
+
+            if(level < 0)
+                level = 1;
+            else
+                _level = level;
+
+            if (health < 0)
+                health = 5;
+            else
+                _health = health;
+        }
+
+        public void ShowInformation() => Console.WriteLine($"Я {_name}, моя сила {_power}, мой уровень {_level} и у меня осталось {_health} жизней");
+    }
+}
---
+++ PlayerInformation/PlayerInformation/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayerInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Атрей", 1000, 4, 100);
+
+            player.ShowInformation();
+        }
+    }
+}
