d07edae91e14a4cb0406857d39186aecac54d1f1
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-02-01 13:05:02 +0300

    Shop programm

---
+++ Collections/Shop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> purchaseAmounts = new Queue<int>(new int[] { 10, 20, 5, 30, 45, 15 });
+
+            int wallet = 0;
+
+            while(purchaseAmounts.Count > 0)
+            {
+                int currentPurchaseAmount = purchaseAmounts.Dequeue();
+
+                Console.WriteLine($"Вы обслужили покупателя на сумму {currentPurchaseAmount}");
+
+                wallet += currentPurchaseAmount;
+
+                Console.WriteLine($"Текущий счет кошелька: {wallet}");
+
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+            Console.WriteLine("Вы обслужили всех покупателей, поздравляю!");
+        }
+    }
+}
