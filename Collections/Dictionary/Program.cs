e25a451de62846668adadf2892be4d34c9d63951
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-02-01 12:54:45 +0300

    Dictionary programm

---
+++ Collections/Dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCommand = "exit";
+
+            Dictionary<string, string> wordToMeaning = new Dictionary<string, string>()
+            {
+                {"арбуз", "это ягода" },
+                {"ютуб", "это видеохостинг" },
+                {"множество", "это совокупность объектов" },
+            };
+
+            bool programmIsRunning = true;
+
+            while (programmIsRunning)
+            {
+                Console.WriteLine("Введите слово и узнайте его значение, для выхода введите exit");
+
+                string input = Console.ReadLine();
+
+                if (wordToMeaning.ContainsKey(input))
+                    Console.WriteLine(wordToMeaning[input]);
+                else
+                    Console.WriteLine("Такого слова нет в словаре:(");
+
+                if (input == exitCommand)
+                    programmIsRunning = false;
+            }
+        }
+    }
+}
