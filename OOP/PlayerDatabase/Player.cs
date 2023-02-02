f947e9509b29e0d5a907b64fbc69fc40ea67cde6
wizard2122 <jackovlev.ilia2018@yandex.ru>
2023-02-02 12:15:42 +0300

    PlayerDatabase edits

--- OOP/PlayerDatabase/Player.cs
+++ OOP/PlayerDatabase/Player.cs
@@ -4,33 +4,28 @@ namespace PlayerDatabase
 {
     public class Player
     {
-        private uint _id;
-        private string _name;
-        private int _level;
-        private bool _isBanned;
-
-        public uint Id => _id;
-        public string Name => _name;
-        public int Level => _level;
-        public bool IsBanned => _isBanned;
+        public uint Id { get; }
+        public string Name { get; }
+        public int Level { get; }
+        public bool IsBanned { get; private set; }
 
         public Player(uint id, string name, int level)
         {
-            _id = id;
-            _name = name;
+            Id = id;
+            Name = name;
 
             if(level < 0)
-                _level = 0;
+                Level = 0;
             else
-                _level = level;
+                Level = level;
 
-            _isBanned = false;
+            IsBanned = false;
         }
 
-        public void Ban() => _isBanned = true;
+        public void Ban() => IsBanned = true;
 
-        public void UnBan() => _isBanned = false;
+        public void UnBan() => IsBanned = false;
 
-        public void ShowInformation() => Console.WriteLine($"id - {_id}, имя - {_name}, уровень - {_level}, бан - {_isBanned}");
+        public void ShowInformation() => Console.WriteLine($"id - {Id}, имя - {Name}, уровень - {Level}, бан - {IsBanned}");
     }
 }
--- OOP/PlayerDatabase/PlayerDatabase.cs
+++ OOP/PlayerDatabase/PlayerDatabase.cs
@@ -1,67 +1,82 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace PlayerDatabase
 {
     public class PlayerDatabase
     {
-        private Dictionary<uint, Player> _idToPlayer;
+        private Dictionary<uint, Player> _players;
 
         public PlayerDatabase(List<Player> players)
         {
-            _idToPlayer = new Dictionary<uint, Player>();
+            _players = new Dictionary<uint, Player>();
 
             foreach (var player in players)
-                _idToPlayer.Add(player.Id, player);;
+                _players.Add(player.Id, player);;
         }
 
-        public PlayerDatabase() => _idToPlayer = new Dictionary<uint, Player>();
+        public PlayerDatabase() => _players = new Dictionary<uint, Player>();
 
         public bool Add(Player player)
         {
-            if (_idToPlayer.ContainsKey(player.Id))
+            if (_players.ContainsKey(player.Id))
+            {
+                Console.WriteLine("Похоже такой игрок уже добавлен в базу данных");
                 return false;
+            }
 
-            _idToPlayer.Add(player.Id, player);
+            _players.Add(player.Id, player);
             return true;
         }
 
         public bool Remove(uint id)
         {
-            if (_idToPlayer.ContainsKey(id))
+            if (_players.ContainsKey(id))
             {
-                _idToPlayer.Remove(id);
+                _players.Remove(id);
                 return true;
             }
 
+            ShowNotFoundIdMessage(id);
             return false;
         }
 
         public bool Ban(uint id)
         {
-            if (_idToPlayer.ContainsKey(id))
+            if (_players.ContainsKey(id))
             {
-                _idToPlayer[id].Ban();
+                _players[id].Ban();
                 return true;
             }
 
+            ShowNotFoundIdMessage(id);
             return false;
         }
 
-        public bool UnBan(uint id)
+        public bool Unban(uint id)
         {
-            if (_idToPlayer.ContainsKey(id))
+            if (_players.ContainsKey(id))
             {
-                _idToPlayer[id].UnBan();
+                _players[id].UnBan();
                 return true;
             }
 
+            ShowNotFoundIdMessage(id);
             return false;
         }
 
         public void ShowInformation()
         {
-            foreach (var item in _idToPlayer)
+            if(_players.Count == 0)
+            {
+                Console.WriteLine("База данных пуста");
+                return;
+            }
+
+            foreach (var item in _players)
                 item.Value.ShowInformation();
         }
+
+        private void ShowNotFoundIdMessage(uint id) => Console.WriteLine($"Игрока с id {id} нет в базе данных");
     }
 }
