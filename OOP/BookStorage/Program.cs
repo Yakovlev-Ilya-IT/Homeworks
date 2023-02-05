a8c58a5e6fe0898674176f4a4266bdf6d5df14b1
Yakovlev-Ilya-IT <yakovlev.youtube>
2023-02-05 13:13:11 +0300

    BookStorage edits

--- OOP/BookStorage/Program.cs
+++ OOP/BookStorage/Program.cs
@@ -5,163 +5,11 @@ namespace BookStorage
 {
     internal class Program
     {
-        private const string AddCommand = "1";
-        private const string ShowSortCommand = "2";
-        private const string ShowAllCommand = "3";
-        private const string ExitCommand = "4";
-
-        private const string SortByAuthorCommand = "1";
-        private const string SortByTitleCommand = "2";
-        private const string SortByReleaseYearCommand = "3";
-
         static void Main(string[] args)
         {
             BookStorage bookStorage = new BookStorage();
 
-            bool isProgramRuning = true;
-
-            while (isProgramRuning)
-            {
-                ShowMainMenu();
-
-                string input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case AddCommand:
-                        AddBook(bookStorage);
-                        break;
-
-                    case ShowSortCommand:
-                        ShowSortedBooks(bookStorage);
-                        break;
-
-                    case ShowAllCommand:
-                        ShowAllBooks(bookStorage);
-                        break;
-
-                    case ExitCommand:
-                        isProgramRuning = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Такой команды нет в списке, попробуйте снова");
-                        break;
-                }
-            }
-        }
-
-        private static void ShowAllBooks(BookStorage bookStorage)
-        {
-            if (CheckCapacity(bookStorage) == false)
-                return;
-
-            bookStorage.ShowAll();
-        }
-
-        private static void ShowSortedBooks(BookStorage bookStorage)
-        {
-            if (CheckCapacity(bookStorage) == false)
-                return;
-
-            ShowSortingMenu();
-
-            string input = Console.ReadLine();
-
-            switch (input)
-            {
-                case SortByAuthorCommand:
-                    ShowSortedBooksByAuthor(bookStorage);
-                    break;
-
-                case SortByTitleCommand:
-                    ShowSortedBooksByTitle(bookStorage);
-                    break;
-
-                case SortByReleaseYearCommand:
-                    ShowSortedBooksByReleaseYear(bookStorage);
-                    break;
-
-                default:
-                    Console.WriteLine("Такой команды нет в списке");
-                    break;
-            }
-        }
-
-        private static void ShowSortedBooksByAuthor(BookStorage bookStorage)
-        {
-            Console.WriteLine("Введите автора");
-
-            string input = Console.ReadLine();
-
-            bookStorage.ShowBy((x, y) => x.Author.ToUpper() == y.ToUpper(), input);
-        }
-
-        private static void ShowSortedBooksByTitle(BookStorage bookStorage)
-        {
-            Console.WriteLine("Введите название");
-
-            string input = Console.ReadLine();
-
-            bookStorage.ShowBy((x, y) => x.Title.ToUpper() == y.ToUpper(), input);
-        }
-
-        private static void ShowSortedBooksByReleaseYear(BookStorage bookStorage)
-        {
-            Console.WriteLine("Введите год выпуска");
-
-            string input = Console.ReadLine();
-
-            bookStorage.ShowBy((x, y) => x.ReleaseYear.ToUpper() == y.ToUpper(), input);
-        }
-
-        private static bool CheckCapacity(BookStorage bookStorage)
-        {
-            if(bookStorage.Count == 0)
-            {
-                Console.WriteLine("Книг в хранилище нет");
-                return false;
-            }
-
-            return true;
-        }
-
-        private static void AddBook(BookStorage bookStorage)
-        {
-            Console.WriteLine("Введите название книги");
-
-            string title = Console.ReadLine();
-
-            Console.WriteLine("Введите имя автора");
-
-            string author = Console.ReadLine();
-
-            Console.WriteLine("Введите год выпуска");
-
-            string releaseYear = Console.ReadLine();
-
-            bookStorage.Add(new Book(title, author, releaseYear));
-        }
-
-        private static void ShowMainMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Выберите одну из команд:");
-            Console.WriteLine($"{AddCommand} - добавить книгу"); 
-            Console.WriteLine($"{ShowSortCommand} - вывести отсортированные книги"); 
-            Console.WriteLine($"{ShowAllCommand} - вывести все книги"); 
-            Console.WriteLine($"{ExitCommand} - выйти");
-            Console.WriteLine();
-        }
-
-        private static void ShowSortingMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("По какому параметру выводить книги?");
-            Console.WriteLine($"{SortByAuthorCommand} - по автору");
-            Console.WriteLine($"{SortByTitleCommand} - по названию");
-            Console.WriteLine($"{SortByReleaseYearCommand} - по году выпуска");
-            Console.WriteLine();
+            bookStorage.Work();
         }
     }
 
@@ -183,31 +31,184 @@ namespace BookStorage
 
     public class BookStorage
     {
-        private List<Book> _books = new List<Book>();
+        private const string AddCommand = "1";
+        private const string RemoveCommand = "2";
+        private const string ShowSortCommand = "3";
+        private const string ShowAllCommand = "4";
+        private const string ExitCommand = "5";
 
-        public int Count => _books.Count;
+        private const string SortByAuthorCommand = "1";
+        private const string SortByTitleCommand = "2";
+        private const string SortByReleaseYearCommand = "3";
 
-        public void Add(Book book) => _books.Add(book);
+        private List<Book> _books;
 
-        public bool TryRemoveAt(int index)
+        public BookStorage()
         {
-            if(index >= _books.Count || index < 0)
+            _books = new List<Book>();
+
+            _books.Add(new Book("Преступление и наказание", "Достоевский", "1865"));
+            _books.Add(new Book("Три товарища", "Ремарк", "1936"));
+            _books.Add(new Book("Властелин колец", "Толкин", "1954"));
+        }
+
+        public void Work()
+        {
+            bool isWork = true;
+
+            while (isWork)
             {
-                Console.WriteLine("Введенный номер книги за пределами хранлища");
-                return false;
+                ShowMainMenu();
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case AddCommand:
+                        AddBook();
+                        break;
+
+                    case RemoveCommand:
+                        RemoveBook();
+                        break;
+
+                    case ShowSortCommand:
+                        ShowSortedBooks();
+                        break;
+
+                    case ShowAllCommand:
+                        ShowAllBooks();
+                        break;
+
+                    case ExitCommand:
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Такой команды нет в списке, попробуйте снова");
+                        break;
+                }
+            }
+        }
+
+        private void ShowMainMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Выберите одну из команд:");
+            Console.WriteLine($"{AddCommand} - добавить книгу");
+            Console.WriteLine($"{RemoveCommand} - убрать книгу по номеру");
+            Console.WriteLine($"{ShowSortCommand} - вывести отсортированные книги");
+            Console.WriteLine($"{ShowAllCommand} - вывести все книги");
+            Console.WriteLine($"{ExitCommand} - выйти");
+            Console.WriteLine();
+        }
+
+        private void ShowSortingMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("По какому параметру выводить книги?");
+            Console.WriteLine($"{SortByAuthorCommand} - по автору");
+            Console.WriteLine($"{SortByTitleCommand} - по названию");
+            Console.WriteLine($"{SortByReleaseYearCommand} - по году выпуска");
+            Console.WriteLine();
+        }
+
+        private void AddBook()
+        {
+            Console.WriteLine("Введите название книги");
+
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Введите имя автора");
+
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Введите год выпуска");
+
+            string releaseYear = Console.ReadLine();
+
+            _books.Add(new Book(title, author, releaseYear));
+        }
+
+        private void RemoveBook()
+        {
+            Console.WriteLine("Введите номер книги, которую хотите удалить");
+
+            if(int.TryParse(Console.ReadLine(), out int bookNumber))
+            {
+                bookNumber -= 1;
+
+                if(bookNumber < 0 || bookNumber >= _books.Count)
+                {
+                    Console.WriteLine("Введенный номер книги за пределами хранлища");
+                    return;
+                }
+
+                _books.RemoveAt(bookNumber);
+                Console.WriteLine("Книга успешно удалена");
+                return;
             }
 
-            _books.RemoveAt(index);
-            return true;
+            Console.WriteLine("Пожалуйста, введите НОМЕР книги");
         }
 
-        public void ShowAll()
+        private void ShowSortedBooks()
         {
-            foreach (Book book in _books)
-                book.ShowInfo();
+            if (CheckCapacity() == false)
+                return;
+
+            ShowSortingMenu();
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case SortByAuthorCommand:
+                    ShowSortedBooksByAuthor();
+                    break;
+
+                case SortByTitleCommand:
+                    ShowSortedBooksByTitle();
+                    break;
+
+                case SortByReleaseYearCommand:
+                    ShowSortedBooksByReleaseYear();
+                    break;
+
+                default:
+                    Console.WriteLine("Такой команды нет в списке");
+                    break;
+            }
         }
 
-        public void ShowBy(Func<Book, string ,bool> comparer, string comparedValue)
+        private void ShowSortedBooksByAuthor()
+        {
+            Console.WriteLine("Введите автора");
+
+            string input = Console.ReadLine();
+
+            ShowBy((x, y) => x.Author.ToUpper() == y.ToUpper(), input);
+        }
+
+        private void ShowSortedBooksByTitle()
+        {
+            Console.WriteLine("Введите название");
+
+            string input = Console.ReadLine();
+
+            ShowBy((x, y) => x.Title.ToUpper() == y.ToUpper(), input);
+        }
+
+        private void ShowSortedBooksByReleaseYear()
+        {
+            Console.WriteLine("Введите год выпуска");
+
+            string input = Console.ReadLine();
+
+            ShowBy((x, y) => x.ReleaseYear.ToUpper() == y.ToUpper(), input);
+        }
+
+        private void ShowBy(Func<Book, string, bool> comparer, string comparedValue)
         {
             int matchCounter = 0;
 
@@ -220,8 +221,36 @@ namespace BookStorage
                 }
             }
 
-            if(matchCounter == 0)
+            if (matchCounter == 0)
                 Console.WriteLine("Таких книг не найдено");
+        }
+
+        private void ShowAllBooks()
+        {
+            if (CheckCapacity() == false)
+                return;
+
+            ShowAll();
+        }
+
+        private bool CheckCapacity()
+        {
+            if (_books.Count == 0)
+            {
+                Console.WriteLine("Книг в хранилище нет");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ShowAll()
+        {
+            for (int i = 0; i < _books.Count; i++)
+            {
+                Console.Write($"{i + 1} -");
+                _books[i].ShowInfo();
+            }
         }
     }
 }
